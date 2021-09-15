c5e1d72ae0e70d2e3e22af67006d9f0821ff7d21
Lindert <jonas.lindert@gmail.com>
2021-09-15 10:29:40 +0100

    public_beta: merge trunk:1772a819

--- scons-local/scons-local-3.0.5/SCons/Tool/gdc.py
+++ scons-local/scons-local-3.0.5/SCons/Tool/gdc.py
@@ -113,7 +113,7 @@ def generate(env):
 
     # __RPATH is set to $_RPATH in the platform specification if that
     # platform supports it.
-    env['RPATHPREFIX'] = '-Wl,-rpath,' if env['PLATFORM'] == 'darwin' else '-L-rpath='
+    env['RPATHPREFIX'] = '-Wl,-rpath,' if env['PLATFORM'] == 'darwin' else '-Wl,-rpath='
     env['RPATHSUFFIX'] = ''
     env['_RPATH'] = '${_concat(RPATHPREFIX, RPATH, RPATHSUFFIX, __env__)}'
 
